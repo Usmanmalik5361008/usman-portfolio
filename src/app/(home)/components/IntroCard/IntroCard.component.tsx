547f7205a1fcf93e7dbb95db44ad1d879dd4338d
Usman Malik <usmanmalik@Usmans-MacBook-Pro.local>
2025-04-25 15:17:39 +0500

    removed image

--- src/app/(home)/components/IntroCard/IntroCard.component.tsx
+++ src/app/(home)/components/IntroCard/IntroCard.component.tsx
@@ -11,20 +11,13 @@ const { Text, Title } = Typography;
 const IntroCard: React.FC = () => {
   return (
     <div className={classes["intro-card"]}>
-      <div>
-        <Image
-          src={"/owner-image.jpg"}
-          alt="owner-image"
-          layout="fill"
-          objectFit="cover"
-          objectPosition="top"
-          style={{ borderRadius: 8 }}
-        />
-      </div>
+      <div></div>
       <div>
         <Text className="subtle-text">A Full Stack Developer</Text>
         <Title className="m-0">Usman Malik</Title>
-        <Text className="subtle-text">I am a Full Stack Developer based in Pakistan</Text>
+        <Text className="subtle-text">
+          I am a Full Stack Developer based in Pakistan
+        </Text>
       </div>
     </div>
   );
