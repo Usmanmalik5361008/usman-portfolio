36d0e5a645269664dffc34ec0c95a583cc2700b3
Usman Malik <usmanmalik@Usmans-MacBook-Pro.local>
2025-04-25 15:36:41 +0500

    linted

--- src/app/(home)/components/index.ts
+++ src/app/(home)/components/index.ts
@@ -1,6 +1,6 @@
-export { default as WorkTogetherCard } from './workTogetherCard/workTogetherCard.component';
-export { default as ProjectCard } from './projectCard/projectCard.component';
-export { default as ProfileCard } from './profileCard/profileCard.component';
-export { default as TagLineCard } from './tagLineCard/tagLineCard.component'
-export { default as NumbersCard } from './numbersCard/numbersCard.component'
-export { default as IntroCard } from './introCard/introCard.component';
+export { default as WorkTogetherCard } from "./WorkTogetherCard/WorkTogetherCard.component";
+export { default as ProjectCard } from "./ProjectCard/projectCard.component";
+export { default as ProfileCard } from "./ProfileCard/ProfileCard.component";
+export { default as TagLineCard } from "./TagLineCard/TagLineCard.component";
+export { default as NumbersCard } from "./NumbersCard/NumbersCard.component";
+export { default as IntroCard } from "./IntroCard/IntroCard.component";
